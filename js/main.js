5c74d5e02d0b493acc767914ddcf747f70c49dc0
Aster Valdemoro <astervaldemoro@Asters-MacBook-Pro.local>
2018-06-18 12:04:16 -0400

    style: formatted index.html to look better

--- js/main.js
+++ js/main.js
@@ -165,15 +165,17 @@ createRestaurantHTML = (restaurant) => {
     name.innerHTML = restaurant.name;
     li.append(name);
 
-    const neighborhood = document.createElement('p');
-    neighborhood.innerHTML = restaurant.neighborhood;
-    li.append(neighborhood);
-
     const address = document.createElement('p');
     address.innerHTML = restaurant.address;
     li.append(address);
 
+    const neighborhood = document.createElement('p');
+    neighborhood.className = 'neighborhood-type';
+    neighborhood.innerHTML = restaurant.neighborhood;
+    li.append(neighborhood);
+
     const more = document.createElement('a');
+    more.id = "more-details";
     more.innerHTML = 'View Details';
     more.href = DBHelper.urlForRestaurant(restaurant);
     li.append(more)
